55a5b950e037df7e30d8d1116d69eea7a42803d0
souvenir-s <tmdqls1015@naver.com>
2023-11-07 10:06:32 +0900

    nav

---
+++ app/src/main/java/com/example/object_orientedprogramming/Friends.kt
@@ -0,0 +1,5 @@
+package com.example.object_orientedprogramming
+
+class Friends {
+
+}
--- app/src/main/java/com/example/object_orientedprogramming/MainActivity.kt
+++ app/src/main/java/com/example/object_orientedprogramming/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.object_orientedprogramming
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//gdgd 왜 안돼
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
