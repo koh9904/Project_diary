8e1953591b09e40fe533381cd90d7ab766e19810
chadoli27 <jasminekim1009@gmail.com>
2023-11-12 06:25:28 +0900

    달력 생성 + 날짜 보이게

--- app/src/main/java/com/example/object_orientedprogramming/MainActivity.kt
+++ app/src/main/java/com/example/object_orientedprogramming/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.object_orientedprogramming
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.object_orientedprogramming.diary.DiaryListFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -10,8 +12,10 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         val bnv = findViewById<BottomNavigationView>(R.id.bnv)
         supportFragmentManager.beginTransaction().add(R.id.fl, MainFragment()).commit()
