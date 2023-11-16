f29f98c24fd74e6bfaa62000448a6570ed3d4678
souvenir-s <tmdqls1015@naver.com>
2023-11-16 15:03:10 +0900

    네비게이션 액션 이용하도록 수정, 일기 화면 제외 화면 연결 완료

---
+++ MyApplication/app/src/main/java/com/example/myapplication/FriendsAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.myapplication
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.databinding.ListFriendsBinding
+import com.example.object_orientedprogramming.EGender
+import com.example.object_orientedprogramming.Friend
+
+class FriendsAdapter(private val friends: Array<Friend>) : RecyclerView.Adapter<FriendsAdapter.Holder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val binding = ListFriendsBinding.inflate(LayoutInflater.from(parent.context))
+        return Holder(binding)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(friends[position])
+    }
+
+    override fun getItemCount() = friends.size
+
+    class Holder(private val binding: ListFriendsBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(friend: Friend) {
+            binding.imageView.setImageResource( when( friend.gender ) {
+                EGender.MALE -> R.drawable.male
+                EGender.FEMALE -> R.drawable.female
+            })
+            binding.txtName.text = friend.name
+            binding.txtIm.text = friend.im
+        }
+    }
+}
