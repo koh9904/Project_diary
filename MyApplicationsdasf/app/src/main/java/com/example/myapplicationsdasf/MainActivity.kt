5a079a21c8b0e8f52db58250727b0fd5071f1c8c
souvenir-s <tmdqls1015@naver.com>
2023-11-16 21:35:20 +0900

    "action"

---
+++ MyApplicationsdasf/app/src/main/java/com/example/myapplicationsdasf/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val navController = binding.frgNav.getFragment<NavHostFragment>().navController
+        setupActionBarWithNavController(navController)
+        binding.bottomNav.setupWithNavController(navController)
+
+        setContentView(binding.root)
+    }
+}
